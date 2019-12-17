86dcc9746a99f940a7634946b635fbd6deb6d660
Dragan Okanovic <dokanovic@deployinc.com>
2019-12-17 16:18:27 +0100

    Example

--- src/6_examples.tsx
+++ src/6_examples.tsx
@@ -3,42 +3,34 @@ import React from 'react';
 
 type ListProps<T> = {
     data: T[]
-    renderOne?: (item: T) => null
+    renderOne?: (item: T) => React.ReactNode
 }
 
 type ListState<T> = {
     data: T[]
 }
 
-class List<T extends {id: number}> extends React.Component<ListProps<T>, ListState<T>> {
+export class List<T extends {id: number}> extends React.Component<ListProps<T>, ListState<T>> {
     state = {
         data: []
     }
+    componentWillMount() {
+        this.setState({
+            ...this.props
+        })
+    }
     render() {
         return (
-            <ul>
-                {
-                    this.state.data.map((item: T) => <li key={item.id}>{this.props.renderOne?this.props.renderOne(item):item.id}</li>)
-                }
-            </ul>
+            <>
+                <div>Lista:</div>
+                <ul>
+                    {
+                        this.state.data.map((item: T) => <li key={item.id}>{this.props.renderOne?this.props.renderOne(item):item.id}</li>)
+                    }
+                </ul>
+            </>
         )
     }
 }
-
-let ljudi = [
-    {
-        id: 0,
-        name: "Dragan"
-    }, {
-        id: 1,
-    name: "Pera"
-    }, {
-        id: 2,
-        name: "djura"
-    }
-]
-// const jsx = <List data={ljudi} />
-
-// function ListFn<T>() {}
 
 // HOMEWORK!!
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import * as examples from './6_examples';
 
 const App: React.FC = () => {
+
+    let ljudi = [
+        {
+            id: 0,
+            name: "Dragan"
+        }, {
+            id: 1,
+        name: "Pera"
+        }, {
+            id: 2,
+            name: "djura"
+        }
+    ]
+
   return (
     <div className="App">
-      Typescript tutorial app
+        <h1>Typescript tutorial app</h1>
+        <div>
+            <examples.List
+                data={ljudi}
+                renderOne={(item) => `Ovo je covek zvani ${item.name}, sa id ${item.id}`}
+            />
+        </div>
     </div>
   );
 }
